e37f5bebeed6a3a87894e8b634cf94b588c9ba3a
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2022-03-06 23:43:45 +0900

    테트리스

--- 테트리스 게임/js/tetris.js
+++ 테트리스 게임/js/tetris.js
@@ -8,12 +8,13 @@ const GAME_COLS = 10;
 init()
 
 function init(){
-    
-
+    tempMovingItem = { ...movingItem }; // {... } : 스프레드 오퍼레이터 -  이걸 하면 movingItem안의 값만 대입을 시킴
     for(let i = 0; i < GAME_ROWS; i++){
         prependNewLine()
     }
+    renderBlocks()
 }
+
 //functions
 function prependNewLine(){
     const li = document.createElement("li");
@@ -26,8 +27,17 @@ function prependNewLine(){
     playground.prepend(li)
 }
 
+//블럭을 렌더링 하는 함수
 function renderBlocks(){
-
+    const {type, direction, top, left} = tempMovingItem;
+    
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0];
+        const y = block[1];
+        const target = playground.childNodes[y].childNodes[0].childNodes[x];
+        target.classList.add(type)
+        
+    });
 }
 
 
@@ -52,6 +62,5 @@ const movingItem = {
     type: "tree",
     direction: 0,
     top: 0,
-    left: 0,
-
-}
+    left: 3,
+};
